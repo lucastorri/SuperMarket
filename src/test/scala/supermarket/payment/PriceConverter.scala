edf12c6081af44d483bd74e6c550a90e8027ef1e
Lucas Torri <lucastorri@gmail.com>
2011-06-19 18:42:41 -0300

    small DSL for testing the CheckoutCounter

--- src/test/scala/supermarket/payment/PriceConverter.scala
+++ src/test/scala/supermarket/payment/PriceConverter.scala
@@ -1,5 +1,17 @@
 package supermarket.payment
 
+import supermarket.ShoppingCart
+import supermarket.payment.{Price, CheckoutCounter}
+
 trait PriceConverter {
   implicit def intTuple2Price(t: (Int, Int)) = new Price(t._1, t._2)
+
+  def whenCheckingOut = new {
+    def in(counter: CheckoutCounter) = new {
+      def theTotalPriceFor(cart: ShoppingCart) = {
+        counter.checkout(cart).getTotalPrice
+      }
+    }
+  }
+
 }
