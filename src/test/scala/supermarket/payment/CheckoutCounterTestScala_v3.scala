9a30e2d9a84b2b801ac140b85c5eb7a885f381da
Lucas Torri <lucastorri@gmail.com>
2011-06-19 17:51:53 -0300

    adding version of CheckoutCounter test in scala that is able to run in parallel

---
+++ src/test/scala/supermarket/payment/CheckoutCounterTestScala_v3.scala
@@ -0,0 +1,31 @@
+package supermarket.payment
+
+import supermarket.{SupermarketItem, ShoppingCart}
+import supermarket.taxes.Tax
+import supermarket.payment.{Price, CheckoutCounter}
+
+import org.specs2.mutable._
+
+class CheckoutCounterTestScala_v3 extends Specification {
+
+  implicit def intTuple2Price(t: (Int, Int)) = new Price(t._1, t._2)
+
+  private val riceItem = new SupermarketItem("Rice", (2,47), PlusOneTaxStub)
+  private val beansItem = new SupermarketItem("Beans", (0,99), PlusOneTaxStub)
+  def counter = new CheckoutCounter
+  def cart = new ShoppingCart().add(riceItem).add(riceItem).add(beansItem).add(beansItem)
+
+  "knows that an empty cart has price zero" in {
+    val bill = counter.checkout(new ShoppingCart)
+    bill.getTotalPrice must be equalTo((0,0))
+  }
+
+  "sums the total price of the items in the cart" in {
+     val bill = counter.checkout(cart)
+     bill.getTotalPrice must be equalTo((10,92))
+  }
+
+  object PlusOneTaxStub extends Tax {
+    def calculate(p: Price) = (1,0)
+  }
+}
