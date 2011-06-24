ce0fa8081bc75bf3f404e026ef26d8efd1b0bb58
Lucas Torri <lucastorri@gmail.com>
2011-06-24 19:53:30 -0300

    adding script to automatically open files used in presentation

---
+++ presentation.sh
@@ -0,0 +1,15 @@
+EDITOR=mate
+
+function editFiles {
+  $EDITOR `find $1 -iname $2 | tr '\\n' ' '`
+}
+BASEPATH=`dirname $0`
+
+
+$EDITOR $BASEPATH/src/main/java/supermarket
+
+editFiles $BASEPATH/src/test/ 'CheckoutCounterTest*'
+
+editFiles $BASEPATH/src/test/ 'CustomerTest*'
+
+$EDITOR $BASEPATH/src/test/scala/other
