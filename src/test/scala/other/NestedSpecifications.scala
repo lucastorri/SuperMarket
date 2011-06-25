f34a4ae9559cc1056df1fbe87a252fc665252b82
Lucas Torri <lucastorri@gmail.com>
2011-06-25 17:33:09 -0300

    comment runwith since idea has specs2 integration, just have to select class name and ctrl+shift+f10

--- src/test/scala/other/NestedSpecifications.scala
+++ src/test/scala/other/NestedSpecifications.scala
@@ -1,9 +1,12 @@
 package other
 
 import org.specs2.mutable._
+/*
 import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
 
-@RunWith(classOf[org.specs2.runner.JUnitRunner])
+@RunWith(classOf[JUnitRunner])
+*/
 class NestedSpecifications extends Specification {
   
   "A shopping cart".title
