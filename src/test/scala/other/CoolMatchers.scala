0f7dd78a1df6262f2cffc0b222f4f582ceeadeb3
Lucas Torri <lucastorri@gmail.com>
2011-06-29 12:30:45 -0300

    adding datatable test example

--- src/test/scala/other/CoolMatchers.scala
+++ src/test/scala/other/CoolMatchers.scala
@@ -2,19 +2,33 @@ package other
 
 import org.specs2.mutable._
 import org.specs2.mock.Mockito
+import org.specs2.matcher.DataTables
 
-class CoolMatchers extends Specification with Tags {
+class CoolMatchers extends Specification with Tags with DataTables {
 
   "more mocking" in {
     success
   }
 
   "data tables" in {
-    success
+    "a" | "b" | "r" |
+     1  !  7  !  8  |
+     3  !  2  !  5  |
+     0  !  9  !  9  |> { (a,b,r) => SuperCalculator.add(a,b) must be equalTo(r) }
   }
 
   "there are tags" in {
     success
   } tag("tagName")
 
+
+  // "I want to fail :(" in {
+  //   failure
+  // }
+
+}
+
+object SuperCalculator {
+
+  def add(a: Int, b: Int) = a + b
 }
